cd901ada5c5a2916d6dd11a292132573a7254290
EbrahimKaram <ebrahim_karam@hotmail.com>
2015-02-21 12:03:59 +0200

    just copied twhat i did last time

--- IC-Studere/IC-Studere/IC-Studere.WindowsPhone/Course selection.xaml.cs
+++ IC-Studere/IC-Studere/IC-Studere.WindowsPhone/Course selection.xaml.cs
@@ -13,20 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
 namespace IC_Studere
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Course_selection : Page
     {
-        public MainPage()
+        public Course_selection()
         {
             this.InitializeComponent();
-
-            this.NavigationCacheMode = NavigationCacheMode.Required;
         }
 
         /// <summary>
@@ -36,24 +34,16 @@ namespace IC_Studere
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            // TODO: Prepare page for display here.
-
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_letsStudere_Click(object sender, RoutedEventArgs e)
         {
-            
-          
+
         }
 
-        private void Button_login_Click(object sender, RoutedEventArgs e)
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Frame.Navigate(typeof(Course_selection));
+
         }
     }
 }
--- IC-Studere/IC-Studere/IC-Studere.WindowsPhone/PickYourQuestionKind.xaml.cs
+++ IC-Studere/IC-Studere/IC-Studere.WindowsPhone/PickYourQuestionKind.xaml.cs
@@ -13,20 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
 namespace IC_Studere
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class PickYourQuestionKind : Page
     {
-        public MainPage()
+        public PickYourQuestionKind()
         {
             this.InitializeComponent();
-
-            this.NavigationCacheMode = NavigationCacheMode.Required;
         }
 
         /// <summary>
@@ -36,24 +34,6 @@ namespace IC_Studere
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            // TODO: Prepare page for display here.
-
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            
-          
-        }
-
-        private void Button_login_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(Course_selection));
         }
     }
 }
